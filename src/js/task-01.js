b2165a2fabda83a89436fa43c650e2b3250afd42
Ekaterina Silchenko <maksil.17@gmail.com>
2022-09-16 17:48:04 +0300

    добавляет решение задачи 1

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const itemCategories = document.querySelectorAll(".item");
+console.log(itemCategories);
+console.log("Number of categories:", itemCategories.length);
+
+itemCategories.forEach((item) => {
+  console.log("Category:", item.firstElementChild.textContent);
+  console.log("Elements:", item.lastElementChild.children.length);
+});
