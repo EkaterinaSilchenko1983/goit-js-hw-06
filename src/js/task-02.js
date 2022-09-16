6f9aae0878c7726c1e2786880d65f216ab922470
Ekaterina Silchenko <maksil.17@gmail.com>
2022-09-16 18:26:15 +0300

    Добавляет task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listIngridients = document.querySelector("#ingredients");
+console.log(listIngridients);
+ingredients.forEach((ingredient) => {
+  const itemIngridient = document.createElement("li");
+  console.log(itemIngridient);
+  itemIngridient.textContent = ingredient;
+  itemIngridient.classList.add("item");
+  listIngridients.append(itemIngridient);
+});
