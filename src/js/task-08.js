365db1c2a6a7d59bc056c4eda5143af960ec097c
Ekaterina Silchenko <maksil.17@gmail.com>
2022-09-17 18:45:43 +0300

    добавляет 8 task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.elements.email.value === "" ||
+    event.currentTarget.elements.password.value === ""
+  ) {
+    alert("Все поля должны быть заполнены");
+  }
+
+  const mail = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  const formData = {
+    mail,
+    password,
+  };
+
+  console.log(formData);
+  form.reset();
+}
