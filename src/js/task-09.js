1627a768be41a415e0454c0f185360952155d033
Ekaterina Silchenko <maksil.17@gmail.com>
2022-09-17 19:57:39 +0300

    добавляет task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+buttonEl.addEventListener("click", onClickButton);
+
+function onClickButton(event) {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+}
