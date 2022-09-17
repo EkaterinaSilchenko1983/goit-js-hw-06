89964e61cc7a4b54ef5f430c37f96ae10f2e8ab4
Ekaterina Silchenko <maksil.17@gmail.com>
2022-09-17 21:24:44 +0300

    добавляет 10 Task

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,6 +3,7 @@ const inputValidEl = document.querySelector("#validation-input");
 inputValidEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
+  event.currentTarget.value = event.currentTarget.value.trim();
   if (event.currentTarget.value.length === 6) {
     inputValidEl.classList.add("valid");
     if (inputValidEl.classList.contains("invalid")) {
