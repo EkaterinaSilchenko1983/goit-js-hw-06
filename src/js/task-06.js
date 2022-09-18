e598d4c2103340e75e43ff34ff1eeda65a83d444
Ekaterina Silchenko <maksil.17@gmail.com>
2022-09-18 14:55:34 +0300

    finish

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,18 +1,13 @@
-const inputValidEl = document.querySelector("#validation-input");
-
 inputValidEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
+  const inputValidEl = document.querySelector("#validation-input");
+  inputValidEl.classList.remove("valid");
+  inputValidEl.classList.remove("invalid");
   event.currentTarget.value = event.currentTarget.value.trim();
   if (event.currentTarget.value.length === 6) {
     inputValidEl.classList.add("valid");
-    if (inputValidEl.classList.contains("invalid")) {
-      inputValidEl.classList.remove("invalid");
-    }
-  } else {
-    inputValidEl.classList.add("invalid");
-    if (inputValidEl.classList.contains("valid")) {
-      inputValidEl.classList.remove("valid");
-    }
+  } else if (event.currentTarget.value.length !== 6) {
+    inputValidEl.classList.add("valid");
   }
 }
--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,7 +1,7 @@
 const inputEl = document.querySelector("#font-size-control");
 const spanEl = document.querySelector("#text");
 
-inputEl.addEventListener("change", onChangeInput);
+inputEl.addEventListener("input", onChangeInput);
 
 function onChangeInput(event) {
   spanEl.style.fontSize = event.currentTarget.value + "px";
